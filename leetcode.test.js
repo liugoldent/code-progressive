ec38febfe273b78033a7f62e68009922bf16e8e5
keedem <liugoldent@gmail.com>
2023-05-25 18:11:16 +0800

    add hard

--- leetcode.test.js
+++ leetcode.test.js
@@ -2,28 +2,33 @@
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function (height) {
-  let maxA = 0
-  let left = 0
-  let right = height.length - 1
-  while (left < right) {
-    maxA = Math.max(maxA, [right - left] * Math.min(height[left], height[right]))
-    if (height[left] > height[right]) {
-      right--
-    } else {
-      left++
+var trap = function(height) {
+    let left = 0
+    let right = height.length -1
+    let leftMax = 0
+    let rightMax = 0
+    let totalWater = 0
+    while(left < right){
+      if(height[left] < height[right]){
+        leftMax = Math.max(leftMax, height[left])
+        totalWater += leftMax - height[left]
+        left++
+      }else{
+        rightMax = Math.max(rightMax, height[right])
+        totalWater += rightMax - height[right]
+        right--
+      }
     }
-  }
-  return maxA
+    return totalWater
 };
 
-test("基本測試", () => {
-  expect(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7])).toEqual(49);
-});
+// test("基本測試", () => {
+//   expect(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7])).toEqual(49);
+// });
 
-test("基本測試", () => {
-  expect(maxArea([1, 1])).toEqual(1);
-});
+// test("基本測試", () => {
+//   expect(maxArea([1, 1])).toEqual(1);
+// });
 
 // test("基本測試", () => {
 //   expect(threeSum([0,0,0])).toMatchObject([[0,0,0]]);
