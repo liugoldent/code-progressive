d307ea76f6f90dd0d213dd5b22f6eb18dc6d62f6
Guan-Ting Liu <liugoldent@gmail.com>
2023-08-06 22:59:00 +0800

    add 0621

--- try.js
+++ try.js
@@ -1,4 +1,4 @@
-let s = '215. Kth Largest Element in an Array'
-console.log(`l0${s.replace(/\s/g, '-').toLocaleLowerCase()}.md`)
+let s = '621. Task Scheduler'
+console.log(`l0${s.replace(/\s/g, '-').toLocaleLowerCase()}.md`.replace('.', ''))
 
 
