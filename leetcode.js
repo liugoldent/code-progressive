e19894687155a9e292930dc3d0cce6713a49ca1c
liugoldent <liugoldent@gmail.com>
2024-02-12 21:38:33 +0800

    add three leetcode

--- leetcode.js
+++ leetcode.js
@@ -1,36 +1,41 @@
 /**
- * @param {number[]} candidates
- * @param {number} target
- * @return {number[][]}
+ * @param {number[]} nums
+ * @return {number}
  */
-var combinationSum = function(candidates, target) {
-  const results = []
-
-  const backTrack = (currentCombination, start, currentSum) => {
-    // 如果顯在的總和就等於target，則return 解構的currentCombination
-    if(currentSum === target){
-      results.push([...currentCombination])
-      return 
+var majorityElement2 = function (nums) {
+  let majorMap = new Map();
+  let countBiggest = 0;
+  let valueBiggest = 0;
+  for (let i = 0, len = nums.length; i < len; i++) {
+    if (!majorMap.has(nums[i])) {
+      majorMap.set(nums[i], 1);
+    } else {
+      let addCount = majorMap.get(nums[i]) + 1;
+      majorMap.set(nums[i], addCount);
     }
-
-    // 如果現在的總和 > target 代表不成立，則return 
-    if(currentSum > target){
-      return 
+    if (majorMap.get(nums[i]) > countBiggest) {
+      countBiggest = majorMap.get(nums[i]);
+      valueBiggest = nums[i];
     }
+  }
+  return valueBiggest;
+};
+var majorityElement = function (nums) {
+  let count = 0;
+  let candidate = null;
 
-    // 遍歷候選數組，從起始索引開始
-    for(let i = start; i < candidates.length ; i ++){
-      currentCombination.push(candidates[i]) // 將候選數加入當前組合
-      backTrack(currentCombination, i, currentSum + candidates[i]) // 遞歸繼續尋找符合要求的組合
-      console.log(currentCombination)
-      currentCombination.pop() // 回溯，將最後一個添加的候選數移出當前組合，進行下一輪遍歷
+  for (let num of nums) {
+    if (count === 0) {
+      candidate = num;
+    }
+    if (num === candidate) {
+      count = count + 1;
+    } else {
+      count = count - 1;
     }
   }
 
-  // 初始化回溯函數，起始組合為空，起始索引為 0，當前總和為 0
-  backTrack([], 0, 0)
-
-  return results
+  return candidate;
 };
 
-combinationSum([2,3,6,7], 7)
+console.log(111, majorityElement([6, 5, 5]));
